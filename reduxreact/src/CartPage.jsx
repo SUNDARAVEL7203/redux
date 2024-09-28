1fab89a674806daec908bb5ad31bedb89fbfce33
SUNDARAVEL7203 <sundarvel.kasi@gmail.com>
2024-09-28 19:00:18 +0530

    redux task

---
+++ reduxreact/src/CartPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Product from "./Components/Product";
+import Summary from "./Components/Summary";
+
+function CartPage() {
+    return (
+      <div>
+        <Product />
+        <Summary />
+      </div>
+    );
+  }
+  
+  export default CartPage;
